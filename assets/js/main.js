f5be32bbac4f371348ae1d06f22df32c801c0946
yana <taratynova.yana@bk.ru>
2024-10-01 10:47:41 +0700

    fix first screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,33 @@ let go=(page, attribute) => {
         if(page !== el) $(`#${el}`).setAttribute('class', 'd-none');
      })
 }
+let checkName = () =>{
+    name = $('#nameInput').value.trim();
+    if (name !== ''){
+        $('#startGame').removeAttribute('disabled');
+        localStorage.setItem('userName', name);
+        //JSON.stringify(name);
+    } else{
+        $('#startGame').setAttribute('disabled', 'disabled');
+    }
+}
+
+let startLoop = () => {
+    let inter = setInterval(() =>{
+        if (panel !== "start") clearInterval(inter);
+        checkName();
+    },100)
+}
+
+let checkStorage =() =>{
+    $('#nameInput').value = localStorage.getItem('userName') || '';
+}
 window.onload = () => {
     nav();
+    checkStorage();
+    startLoop();
     let inter = setInterval(()=> {
-        if(pannel==='game'){
+        if(panel === 'game'){
             game= new Game();
             game.start();
             panel ='game process';
